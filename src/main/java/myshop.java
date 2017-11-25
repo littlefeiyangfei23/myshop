015032ea0cd7aba738aea31fe826840ec6f5e123
stargazor <350222336@qq.com>
2017-11-25 14:34:25 +0800

    myshop1

---
+++ src/main/java/myshop.java
@@ -0,0 +1,11 @@
+/**
+ * User: Administrator
+ * Date: 2017/11/25
+ * Time: 14:32
+ * Version:V1.0
+ */
+public class myshop {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
